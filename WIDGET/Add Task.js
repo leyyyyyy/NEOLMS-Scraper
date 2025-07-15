85f9d95848cc0549e54306f232d0cb0fa6ec732d
leyyyyyy <85753649+leyyyyyy@users.noreply.github.com>
2025-07-15 07:14:19 -0700

    add ios widget

---
+++ WIDGET/Add Task.js
@@ -0,0 +1,22 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: light-gray; icon-glyph: magic;
+
+
+alert = new Alert("Add task")
+alert.message = "Add task"
+alert.addTextField("task name", "")
+
+alert.addDestructiveAction("Cancel")
+alert.addAction("Add")
+alertIndex = await alert.present()
+console.log(alert.textFieldValue())
+if (alertIndex==1){
+  var poop = new Request(API_URL)
+poop.headers = {"taskName": alert.textFieldValue()}
+var response = await poop.loadJSON()
+  
+}
+  
+
+
