3a733a7e685fd32d347dc88695d6417450ed5f5a
ley <zenielsungaxtra@gmail.com>
2025-07-17 10:49:38 -0700

    debugged logic

--- scraper.py
+++ scraper.py
@@ -39,15 +39,36 @@ def findAssignments():
     # Cookies
     # for cookie in session.cookies:
     #     print(f"{cookie.name} = {cookie.value}")
+    data = []
 
     # Getting Assignments
     check_url = 'https://dlsudshs.edu20.org/user_to_do_widget/tasks'
     response = session.get(check_url, headers=headers, cookies=session.cookies, allow_redirects=True)
-
+    output = open("output.html", "w")
+    output.write(response.text)
     # Parsing Assignments
     soup = BeautifulSoup(response.text, 'html.parser')
     subjectList = soup.find_all('a',attrs={'class':'title_and_count'})
-    data = []
+
+
+    tasksUl = soup.find('ul')
+    tasksLi = tasksUl.find_all("li", id=lambda value: value and value.startswith("todo"))
+    print("tasksLi")
+    print(tasksLi)
+    if tasksLi:
+        selfData = []
+        for task in tasksLi:
+            selfData.append({"task": task.find('span').text})
+
+            print(task.find('span').text)
+            print('\n\n\n\n poopy')
+        
+        data.append({
+                    "subject": "Self",
+                    "count": len(tasksLi),
+                    "tasks": selfData
+                        })
+
     for subject in subjectList:
         subjectDataRaw = subject.find_all('span')
         assignUrl = "https://dlsudshs.edu20.org" + subject['href'] 
@@ -101,6 +122,40 @@ def findAssignments():
     return data
 
 findAssignments()
+def addAssignment(name):
 
+    session = rq.Session()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+
+    homeUrl = "https://dlsudshs.edu20.org/log_in/form"
+    loginUrl = 'https://dlsudshs.edu20.org/log_in/submit_from_portal?original_host=dlsudshs.edu20.org'
+
+
+    # Getting creds
+    with open('credentials.json', 'r') as file:
+        creds = json.load(file)
+
+    # Finding Auth Token
+    response = session.get(homeUrl)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tokenElements = soup.find_all("input", attrs={"name": "authenticity_token"})
+    token = tokenElements[0].get('value')
+
+    # Logging in
+    payload = {
+        'userid': creds['User'],
+        'password': creds["Pass"],
+        'authenticity_token': token
+    }
+    response = session.post(loginUrl, headers=headers, params=payload)
+    createUrl = "https://dlsudshs.edu20.org/task/create"
+    response = session.post(createUrl, headers=headers, cookies=session.cookies, allow_redirects=True, data = {"task[description]": name, "authenticity_token": token})
+    print(response)
+
+# addAssignment("I wish you were here.")
+
+# findAssignments()
 # TO DO:
 # NOTION INTEGRATION, CHECK FOR EXISTING TO STOP DUPLICATES
